cdc193525e4ab661f02447cb537972f7ea14abe9
Crowdbotics <team@crowdbotics.com>
2022-12-29 18:39:06 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/networking_template_38082/MainActivity.java
@@ -0,0 +1,15 @@
+package com.networking_template_38082;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "networking_template_38082";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "networking_template_38082",
+  url: "https://networking_template_38082.botics.co",
+  api: "https://networking_template_38082.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
